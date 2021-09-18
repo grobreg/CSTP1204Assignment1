94d0a95f618982530cdfbdfcd3833428ea4d5f17
cst013 <cst013@CC145XX1A012XXX>
2021-09-18 16:24:44 -0700

    Add project files.

---
+++ CSTP1204Assignment1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSTP_1204___Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 100; i++)
+
+                Console.WriteLine("{0}\n", i);
+        }
+    }
+}
